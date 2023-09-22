d299f4220f7116f3f2a28ad18f31201f9f2185a0
orgadish <48453207+orgadish@users.noreply.github.com>
2023-09-21 23:18:47 -0400

    Added clarifications to create_extdata_from_iris.

--- inst/extdata/create_extdata_from_iris.R
+++ inst/extdata/create_extdata_from_iris.R
@@ -1,21 +1,27 @@
+### ---- THIS FILE WAS USED TO CREATE THE EXAMPLE DATA IN THE EXTDATA FOLDER ---- ###
+### It should not need re-running.
 
 # Save the iris dataset as a single CSV and as CSVs per Species, for testing ----------------------------------------------------
 write_to_extdata <- function(tbl, label) {
-  readr::write_csv(
-    tbl,
-    fs::path(
-      fs::path_package("extdata", package="filecacher"),
-      glue::glue("iris_{label}"), ext="csv"
-    )
-  )
+  extdata_folder_path <- fs::path_package("extdata", package="filecacher")
+  file_path <- fs::path(paste0("iris_", label), ext="csv")
+
+  readr::write_csv(tbl, file_path)
 }
 
 create_example_data <- function() {
-  iris |>
-    write_to_extdata("complete")
+  # Create a single file containing the complete data set.
+  write_to_extdata(iris, "complete")
 
+  write_subset_to_extdata <- function(subset_data) {
+    species_name <- unique(subset_data$Species)
+    label <- paste0(species_name, "_only")
+    write_to_extdata(subset_data, label)
+  }
+
+  # Create individual files for each subset.
   dplyr::group_split(iris, Species) |>
-    lapply(\(tbl) write_to_extdata(tbl, glue::glue("{unique(tbl$Species)}_only")))
+    lapply(write_subset_to_extdata)
 }
 
 create_example_data()

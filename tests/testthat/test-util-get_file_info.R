077707ad23bad7419cba61510b813359ae1e8d30
orgadish <48453207+orgadish@users.noreply.github.com>
2023-11-09 00:03:50 -0800

    Update test-util-get_file_info to only check mtime up to milliseconds (to address error in `check_win_devel()`).

--- tests/testthat/test-util-get_file_info.R
+++ tests/testthat/test-util-get_file_info.R
@@ -12,8 +12,12 @@ test_that("get_file_info works", {
     iris_complete_info$size,
     as.character(as.numeric(fs::file_size(iris_complete_path)))
   )
+
+  # Only check `mtime` up to milliseconds:
+  #   xxxx-xx-xx xx:xx:xx.xxx
   expect_equal(
-    iris_complete_info$mtime,
-    as.character(fs::file_info(iris_complete_path)$modification_time)
+    iris_complete_info$mtime |> substr(1, 23),
+    as.character(fs::file_info(iris_complete_path)$modification_time) |>
+      substr(1, 23)
   )
 })

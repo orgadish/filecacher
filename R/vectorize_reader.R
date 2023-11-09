4f288b4533319cced724714f8209471fd473fcfa
orgadish <48453207+orgadish@users.noreply.github.com>
2023-11-08 23:36:47 -0800

    Fix bug in setNames->set_names transition.

--- R/vectorize_reader.R
+++ R/vectorize_reader.R
@@ -23,7 +23,7 @@
 #' @example inst/examples/vectorize_reader.R
 vectorize_reader <- function(read_fn, file_path_to = NULL) {
   function(files, ...) {
-    df_list <- purrr::map(rlang::set_names(nm = files), read_fn, ...)
+    df_list <- purrr::map(rlang::set_names(files), read_fn, ...)
 
     if (is.null(file_path_to)) file_path_to <- rlang::zap()
     purrr::list_rbind(df_list, names_to = file_path_to)
